dcb6b84f76f86ef2c08f62a1d18fb8efc32a804e
alex-dx <778764930@qq.com>
2022-03-25 18:18:50 +0800

    异常处理导包

--- xebs-auth/src/main/java/cc/alex/xebs/handler/GlobalExceptionHandler.java
+++ xebs-auth/src/main/java/cc/alex/xebs/handler/GlobalExceptionHandler.java
@@ -1,5 +1,9 @@
 package cc.alex.xebs.handler;
 
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
 @RestControllerAdvice
 @Order(value = Ordered.HIGHEST_PRECEDENCE)
 public class GlobalExceptionHandler extends BaseExceptionHandler {
